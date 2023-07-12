6bcf450f7036f876e2684856ef00aa42f8d67f36
DivyaMishra6 <divyamishra599@gmail.com>
2023-07-12 18:22:32 +0530

    [Divya]: Add Gender Column

--- SQLEmployeePayroll.sql
+++ SQLEmployeePayroll.sql
@@ -14,10 +14,10 @@ create table employee_payroll
 
 select * from employee_payroll
 
-insert into employee_payroll values('Ram', 50000, '2023-04-21'),
-                                   ('Shyam', 20000, '2023-04-21'),
-								   ('Bhole', 70000, '2023-04-21'),
-								   ('Durga', 50000, '2023-04-21')
+insert into employee_payroll values('Ram', 50000, '2023-07-12'),
+                                   ('Shyam', 20000, '2023-07-11'),
+								   ('Bhole', 70000, '2023-07-13'),
+								   ('Durga', 50000, '2023-07-14')
 
 select * from employee_payroll  /* retrive data */
 
@@ -25,9 +25,9 @@ select Salary from employee_payroll where id = 3   /* salary of particular emplo
 
 select * from employee_payroll where StartDate BETWEEN CAST('2023-07-11' AS DATE) AND GETDATE() /* employee deatil b/w date range */
 
-update employee_payroll set StartDate = '2023-07-12'  where id = 1 /* upadte date */
-update employee_payroll set StartDate = '2023-07-11'  where id = 2
-update employee_payroll set StartDate = '2023-07-13'  where id = 3
-update employee_payroll set StartDate = '2023-07-14'  where id = 4
+alter table employee_payroll
+add Gender char(1)
+update employee_payroll set Gender= 'M' where name ='Ram' or name = 'Shyam' or name = 'Bhole'
+update employee_payroll set Gender= 'F' where name ='Durga'
 
 
